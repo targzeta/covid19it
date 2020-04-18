5a72a4ed561dc354da1eb655c8133a3ce996b949
Emanuele Tomasi <targzeta@gmail.com>
2020-04-18 15:07:53 +0200

    [completion] aggiunto check!

--- complete/complete.bash
+++ complete/complete.bash
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+DATA_DIR=data
+FILE_REGIONI=${DATA_DIR}/dati-regioni/dpc-covid19-ita-regioni.csv
+FILE_PROVINCE=${DATA_DIR}/dati-province/dpc-covid19-ita-province.csv
+COMMAND=covid19it
+
+if [ ! -f $FILE_REGIONI -o ! -f $FILE_PROVINCE ]
+then
+  echo "Dati non presenti. Eseguire $COMMAND almeno una volta!"
+  return
+fi
+
 list="Italia"
 while read regione
 do
@@ -10,4 +23,4 @@ do
 done < <(cut -d',' -f6 data/dati-province/dpc-covid19-ita-province-latest.csv | sed '1d;/In fase di definizione/d;p')
 
 
-complete -W "$list" -P\" -S\" covid19
+complete -W "$list" -P\" -S\" $COMMAND
